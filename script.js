0b635b18f944133f758fe926bffed34bb09e8b4e
Ritwik <shawritwik2@gmail.com>
2022-02-08 11:51:20 +0530

    Show single meal page

--- script.js
+++ script.js
@@ -48,5 +48,63 @@ function searchMeal(e) {
   }
 }
 
+// Fetch meal by ID
+function getMealById(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    });
+}
+
+// Add meal to DOM
+function addMealToDOM(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  single_mealEl.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<h3>${meal.strCategory}</h3>` : ""}
+        ${meal.strArea ? `<h3>${meal.strArea}</h3>` : ""}
+      </div>
+      <div class="main">
+        <h3>ingredients</h3>
+        <ul>
+          ${ingredients.map((ing) => `<li>${ing}</li>`).join("")}
+        </ul>
+        <p>${meal.strInstructions}</p>
+      </div>
+    </div>
+  `;
+}
+
 //Event Listeners
 submit.addEventListener("submit", searchMeal);
+mealsEl.addEventListener("click", (e) => {
+  const mealInfo = e.path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains("meal-info");
+    } else {
+      return false;
+    }
+  });
+
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute("data-mealid");
+    getMealById(mealID);
+  }
+});
